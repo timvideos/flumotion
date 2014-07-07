42b855516f3b4e7e5bb68dace3f1f0003b0af263
Amanpreet Singh <aps.sids@gmail.com>
2014-07-07 08:47:16 -0700

    Ported FGDP component

--- flumotion/component/common/fgdp/fgdp.py
+++ flumotion/component/common/fgdp/fgdp.py
@@ -15,8 +15,9 @@
 #
 # Headers in this file shall remain intact.
 
-import gobject
-import gst
+from gi.repository import GObject
+from gi.repository import Gst
+from gi.repository import GLib
 
 from twisted.internet import reactor
 
@@ -25,12 +26,13 @@ from flumotion.component.common.fgdp import protocol as fgdp
 GDP_TYPE_PRODUCER = "producer-type"
 GDP_TYPE_CONSUMER = "consumer-type"
 
+Gst.init(None)
 
 class FDHandler(object):
     """
     Base class for elements handling file descriptors
 
-    @type  fdelement: L{gst.Element}
+    @type  fdelement: L{Gst.Element}
     """
 
     def __init__(self, fdelement):
@@ -73,10 +75,10 @@ class FDSrc(FDHandler):
         FDHandler.__init__(self, fdelement)
 
     def _check_eos(self, pad, event):
-        if event.type == gst.EVENT_EOS:
+        if event.get_event().type == Gst.EventType.EOS:
             # EOS are triggered after a disconnection, when the read in the
             # socket is 0 Bytes. Remove the handler and close the connection
-            pad.remove_event_probe(self._handler_id)
+            pad.remove_probe(self._handler_id)
             if self.protocol is not None:
                 reactor.callFromThread(self.protocol.loseConnection)
             return False
@@ -87,14 +89,14 @@ class FDSrc(FDHandler):
         # state. Add the fd and an event probe to detect disconnections
         self.fdelement.set_locked_state(False)
         self.fdelement.set_property('fd', fd)
-        srcpad = self.fdelement.get_pad("src")
-        self._handler_id = srcpad.add_event_probe(self._check_eos)
-        self.fdelement.set_state(gst.STATE_PLAYING)
+        srcpad = self.fdelement.get_static_pad("src")
+        self._handler_id = srcpad.add_probe(Gst.PadProbeType.EVENT_BOTH, self._check_eos, None)
+        self.fdelement.set_state(Gst.State.PLAYING)
 
     def disconnectFd(self, _):
         # Set back the element to the READY state, in which a fd can be
         # added/changed and lock the state of the element
-        self.fdelement.set_state(gst.STATE_READY)
+        self.fdelement.set_state(Gst.State.READY)
         self.fdelement.get_state(0)
         self.fdelement.set_locked_state(True)
 
@@ -170,17 +172,17 @@ class _ProtocolMixin(object):
             self._connector.disconnect()
 
     def _start_push(self):
-        self.info("Starting fgdp client")
+        #self.info("Starting fgdp client")
         factory = fgdp.FGDPClientFactory(self)
         self._connector = reactor.connectTCP(self.host, self.port, factory)
 
     def _start_pull(self):
-        self.info("Starting fgdp server")
+        #self.info("Starting fgdp server")
         factory = fgdp.FGDPServerFactory(self)
         self._listener = reactor.listenTCP(self.port, factory)
 
 
-class FGDPBase(gst.Bin, _ProtocolMixin):
+class FGDPBase(Gst.Bin, _ProtocolMixin):
     """
     Base class for gstreamer elements using the FGDP protocol
     """
@@ -194,54 +196,54 @@ class FGDPBase(gst.Bin, _ProtocolMixin):
     version = '0.1'
 
     __gproperties__ = {
-        'mode': (gobject.TYPE_STRING, 'mode',
+        'mode': (GObject.TYPE_STRING, 'mode',
             "Connection mode: 'pull' or 'push'",
-            'pull', gobject.PARAM_READWRITE),
-        'host': (gobject.TYPE_STRING, 'host',
+            'pull', GObject.PARAM_READWRITE),
+        'host': (GObject.TYPE_STRING, 'host',
             'Name of the host to connect (in push mode)',
-            'localhost', gobject.PARAM_READWRITE),
-        'port': (gobject.TYPE_INT, 'port',
+            'localhost', GObject.PARAM_READWRITE),
+        'port': (GObject.TYPE_INT, 'port',
             'Connection port',
-            1, 64000, 15000, gobject.PARAM_READWRITE),
-        'username': (gobject.TYPE_STRING, 'user name',
+            1, 64000, 15000, GObject.PARAM_READWRITE),
+        'username': (GObject.TYPE_STRING, 'user name',
             'Username for the authentication',
-            'user', gobject.PARAM_READWRITE),
-        'password': (gobject.TYPE_STRING, 'password',
+            'user', GObject.PARAM_READWRITE),
+        'password': (GObject.TYPE_STRING, 'password',
             'Password for the authentication',
-            'test', gobject.PARAM_READWRITE),
-        'version': (gobject.TYPE_STRING, 'version',
+            'test', GObject.PARAM_READWRITE),
+        'version': (GObject.TYPE_STRING, 'version',
             'Protocol version',
-            '0.1', gobject.PARAM_READWRITE),
-        'max-reconnection-delay': (gobject.TYPE_FLOAT,
+            '0.1', GObject.PARAM_READWRITE),
+        'max-reconnection-delay': (GObject.TYPE_FLOAT,
             'maximum delay between reconnections in seconds',
             'Maximum delay between reconnections in seconds (for push mode)',
-            1, 100, 5, gobject.PARAM_READWRITE)}
+            1, 100, 5, GObject.PARAM_READWRITE)}
 
-    __gsignals__ = {"connected": (gobject.SIGNAL_RUN_LAST,\
-                                  gobject.TYPE_NONE, []),
-                    "disconnected": (gobject.SIGNAL_RUN_LAST,\
-                                     gobject.TYPE_NONE,
-                                     (gobject.TYPE_STRING, ))}
+    __gsignals__ = {"connected": (GObject.SignalFlags.RUN_LAST,\
+                                  None, []),
+                    "disconnected": (GObject.SignalFlags.RUN_LAST,\
+                                     None,
+                                     (GObject.TYPE_STRING, ))}
 
     def _handle_error(self, message):
-        err = gst.GError(gst.RESOURCE_ERROR,
-                         gst.RESOURCE_ERROR_FAILED, message)
-        m = gst.message_new_error(self, err, message)
+        err = GLib.GError(Gst.resource_error_quark(),
+                         Gst.ResourceError.FAILED, message)
+        m = Gst.Message.new_error(self, err, message)
         self.post_message(m)
         self.error(message)
 
     def do_change_state(self, transition):
-        if transition == gst.STATE_CHANGE_READY_TO_PAUSED:
+        if transition == Gst.StateChange.READY_TO_PAUSED:
             try:
                 self.prepare()
                 self.start()
             except Exception, e:
                 self._handle_error(str(e))
                 self.stop()
-                return gst.STATE_CHANGE_FAILURE
-        elif transition == gst.STATE_CHANGE_PAUSED_TO_READY:
+                return Gst.StateChange.FAILURE
+        elif transition == Gst.StateChange.PAUSED_TO_READY:
             self.stop()
-        return gst.Bin.do_change_state(self, transition)
+        return Gst.Bin.do_change_state(self, transition)
 
     def do_set_property(self, prop, value):
         if prop.name in ['mode', 'host', 'username', 'password', 'port',
@@ -282,13 +284,12 @@ class FGDPSink(FGDPBase, MultiFDSink):
     def __init__(self):
         FGDPBase.__init__(self)
         # Create elements
-        gdppay = gst.element_factory_make('gdppay')
-        self.fdelement = gst.element_factory_make('multifdsink')
+        gdppay = Gst.ElementFactory.make('gdppay')
+        self.fdelement = Gst.ElementFactory.make('multifdsink')
         # Set default properties
         self.fdelement.set_property('sync', False)
-        self.fdelement.set_property('unit-type', 2)
-        self.fdelement.set_property('units-max', 1 * gst.SECOND)
-        self.fdelement.set_property('units-soft-max', 700 * gst.MSECOND)
+        self.fdelement.set_property('units-max', 1 * Gst.SECOND)
+        self.fdelement.set_property('units-soft-max', 700 * Gst.MSECOND)
         self.fdelement.set_property('recover-policy', 1)
         # Create fd handler proxy
         MultiFDSink.__init__(self, self.fdelement)
@@ -296,7 +297,7 @@ class FGDPSink(FGDPBase, MultiFDSink):
         self.add(gdppay, self.fdelement)
         gdppay.link(self.fdelement)
         # Create sink pads
-        self._sink_pad = gst.GhostPad('sink', gdppay.get_pad('sink'))
+        self._sink_pad = Gst.GhostPad.new('sink', gdppay.get_static_pad('sink'))
         self.add_pad(self._sink_pad)
 
 
@@ -314,15 +315,15 @@ class FGDPSrc(FGDPBase, FDSrc):
     def __init__(self):
         FGDPBase.__init__(self)
         # Create elements
-        self.fdelement = gst.element_factory_make('fdsrc')
-        gdpdepay = gst.element_factory_make('gdpdepay')
+        self.fdelement = Gst.ElementFactory.make('fdsrc')
+        gdpdepay = Gst.ElementFactory.make('gdpdepay')
         # Add elements to the bin and link them
         self.add(self.fdelement, gdpdepay)
         self.fdelement.link(gdpdepay)
         # Create fd handler proxy
         FDSrc.__init__(self, self.fdelement)
         # Create sink pads
-        self._src_pad = gst.GhostPad('src', gdpdepay.get_pad('src'))
+        self._src_pad = Gst.GhostPad.new('src', gdpdepay.get_static_pad('src'))
         self.add_pad(self._src_pad)
 
     def prepare(self):
@@ -331,7 +332,38 @@ class FGDPSrc(FGDPBase, FDSrc):
         self.fdelement.set_locked_state(True)
 
 
-gobject.type_register(FGDPSink)
-gst.element_register(FGDPSink, "fgdpsink", gst.RANK_MARGINAL)
-gobject.type_register(FGDPSrc)
-gst.element_register(FGDPSrc, "fgdpsrc", gst.RANK_MARGINAL)
+def plugin_init(plugin, userarg):
+    name = plugin.get_name()
+    pluginType = GObject.type_register(userarg)
+    Gst.Element.register(plugin, name, Gst.Rank.MARGINAL, pluginType)
+    return True
+
+version = Gst.version()
+
+Gst.Plugin.register_static_full(
+    version[0],  # GST_VERSION_MAJOR
+    version[1],  # GST_VERSION_MINOR
+    'fgdpsink',
+    'fgdp sink plugin',
+    plugin_init,
+    '12.06',
+    'LGPL',
+    'fgdp',
+    'fgdp',
+    '',
+    FGDPSink,
+)
+
+Gst.Plugin.register_static_full(
+    version[0],  # GST_VERSION_MAJOR
+    version[1],  # GST_VERSION_MINOR
+    'fgdpsrc',
+    'fgdp src plugin',
+    plugin_init,
+    '12.06',
+    'LGPL',
+    'fgdp',
+    'fgdp',
+    '',
+    FGDPSrc,
+)
